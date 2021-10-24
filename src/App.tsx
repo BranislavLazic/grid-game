293298f001f22a0848f08e205cc87d63b37884b6
Branislav Lazic <brano2411@hotmail.com>
2021-10-24 22:58:37 +0200

    Add slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
 import React, { useState } from 'react';
-import styles from './App.module.css';
 import { Grid } from './components/Grid';
+import Slider from 'rc-slider';
 import { GridProvider } from './context/GridProvider';
+import styles from './App.module.css';
+import 'rc-slider/assets/index.css';
 
 function App() {
   const generateRandomGrid = (size: number) => {
     let grid = [];
-    for (let i = 0; i < 5; i++) {
-      let randomRow = Math.floor(Math.random() * 31 + 1).toString(2);
-      while (randomRow.length < size) {
-        randomRow = '0' + randomRow;
+    for (let i = 0; i < size; i++) {
+      let randomRowValues = Math.floor(
+        Math.random() * Math.pow(2, size)
+      ).toString(2);
+      while (randomRowValues.length < size) {
+        randomRowValues = '0' + randomRowValues;
       }
-      const row = [...randomRow].map((value) => Number(value));
+      const row = [...randomRowValues].map((value) => Number(value));
       grid.push(row);
     }
     return grid;
   };
 
-  const [grid, setGrid] = useState(generateRandomGrid(5));
+  const [grid, setGrid] = useState(generateRandomGrid(10));
 
   const handleRandomize = () => {
-    setGrid(generateRandomGrid(5));
+    setGrid(generateRandomGrid(10));
   };
 
   return (
     <GridProvider>
       <div className={styles.mainContainer}>
-        <h2>Grid game</h2>
-        <Grid grid={grid} />
-        <button onClick={handleRandomize}>Randomize</button>
+        <div className={styles.innerContainer}>
+          <h2>Grid game</h2>
+          <Grid grid={grid} />
+          <Slider min={5} max={10} step={1} />
+          <button onClick={handleRandomize}>Randomize</button>
+        </div>
       </div>
     </GridProvider>
   );
